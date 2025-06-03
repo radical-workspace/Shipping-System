cb295ad7ad94bace8620ed52b8a72cc4d1912e4e
S_Zeyad_E <zeyadana2020@gmail.com>
2025-06-03 17:22:39 +0300

    Update project structure / Add Database Context

---
+++ DAL/DbContext/ShippingContext.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using DAL.UserModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Domains;
+
+namespace DAL.DbContext;
+
+public partial class ShippingContext : IdentityDbContext<ApplicationUser>
+{
+    public ShippingContext()
+    {
+    }
+
+    public ShippingContext(DbContextOptions<ShippingContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Log> Logs { get; set; }
+
+    public virtual DbSet<TbCarrier> TbCarriers { get; set; }
+
+    public virtual DbSet<TbCity> TbCities { get; set; }
+
+    public virtual DbSet<TbCountry> TbCountries { get; set; }
+
+    public virtual DbSet<TbPaymentMethod> TbPaymentMethods { get; set; }
+
+    public virtual DbSet<TbSetting> TbSettings { get; set; }
+
+    public virtual DbSet<TbShippingType> TbShippingTypes { get; set; }
+
+    public virtual DbSet<TbShippment> TbShippments { get; set; }
+
+    public virtual DbSet<TbShippmentStatus> TbShippmentStatuses { get; set; }
+
+    public virtual DbSet<TbSubscriptionPackage> TbSubscriptionPackages { get; set; }
+
+    public virtual DbSet<TbUserReceiver> TbUserReceivers { get; set; }
+
+    public virtual DbSet<TbUserSender> TbUserSenders { get; set; }
+
+    public virtual DbSet<TbUserSubscription> TbUserSubscriptions { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-G1UB6VA; Database=Shipping;Trusted_Connection=True; TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.UseCollation("SQL_Latin1_General_CP1_CI_AS");
+
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Log>(entity =>
+        {
+            entity.ToTable("Log");
+
+            entity.Property(e => e.TimeStamp).HasColumnType("datetime");
+        });
+
+        modelBuilder.Entity<TbCarrier>(entity =>
+        {
+            entity.Property(e => e.Id).HasDefaultValueSql("(newid())");
+            entity.Property(e => e.CarrierName).HasMaxLength(200);
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.UpdatedDate).HasColumnType("datetime");
+        });
+
+        modelBuilder.Entity<TbCity>(entity =>
+        {
+            entity.Property(e => e.Id).HasDefaultValueSql("(newid())");
+            entity.Property(e => e.CityAname)
+                .HasMaxLength(10)
+                .IsFixedLength()
+                .HasColumnName("CityAName");
+            entity.Property(e => e.CityEname)
+                .HasMaxLength(10)
+                .IsFixedLength()
+                .HasColumnName("CityEName");
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.UpdatedDate).HasColumnType("datetime");
+
+            entity.HasOne(d => d.Country).WithMany(p => p.TbCities)
+                .HasForeignKey(d => d.CountryId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_TbCities_TbCountries");
+        });
+
+        modelBuilder.Entity<TbCountry>(entity =>
+        {
+            entity.Property(e => e.Id).HasDefaultValueSql("(newid())");
+            entity.Property(e => e.CountryAname)
+                .HasMaxLength(200)
+                .HasColumnName("CountryAName");
+            entity.Property(e => e.CountryEname)
+                .HasMaxLength(200)
+                .HasColumnName("CountryEName");
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.UpdatedDate).HasColumnType("datetime");
+        });
+
+        modelBuilder.Entity<TbPaymentMethod>(entity =>
+        {
+            entity.Property(e => e.Id).HasDefaultValueSql("(newid())");
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.MethdAname)
+                .HasMaxLength(200)
+                .HasColumnName("MethdAName");
+            entity.Property(e => e.MethodEname)
+                .HasMaxLength(200)
+                .HasColumnName("MethodEName");
+            entity.Property(e => e.UpdatedDate).HasColumnType("datetime");
+        });
+
+        modelBuilder.Entity<TbSetting>(entity =>
+        {
+            entity.ToTable("TbSetting");
+
+            entity.Property(e => e.Id).HasDefaultValueSql("(newid())");
+        });
+
+        modelBuilder.Entity<TbShippingType>(entity =>
+        {
+            entity.Property(e => e.Id).HasDefaultValueSql("(newid())");
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.ShippingTypeAname)
+                .HasMaxLength(200)
+                .HasColumnName("ShippingTypeAName");
+            entity.Property(e => e.ShippingTypeEname)
+                .HasMaxLength(200)
+                .HasColumnName("ShippingTypeEName");
+            entity.Property(e => e.UpdatedDate).HasColumnType("datetime");
+        });
+
+        modelBuilder.Entity<TbShippment>(entity =>
+        {
+            entity.Property(e => e.Id).HasDefaultValueSql("(newid())");
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.PackageValue).HasColumnType("decimal(8, 4)");
+            entity.Property(e => e.ShippingDate).HasColumnType("datetime");
+            entity.Property(e => e.ShippingRate).HasColumnType("decimal(8, 4)");
+            entity.Property(e => e.UpdatedDate).HasColumnType("datetime");
+
+            entity.HasOne(d => d.PaymentMethod).WithMany(p => p.TbShippments)
+                .HasForeignKey(d => d.PaymentMethodId)
+                .HasConstraintName("FK_TbShippments_TbPaymentMethods");
+
+            entity.HasOne(d => d.Receiver).WithMany(p => p.TbShippments)
+                .HasForeignKey(d => d.ReceiverId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_TbShippments_TbUserReceivers");
+
+            entity.HasOne(d => d.Sender).WithMany(p => p.TbShippments)
+                .HasForeignKey(d => d.SenderId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_TbShippments_TbUserSebders");
+
+            entity.HasOne(d => d.ShippingType).WithMany(p => p.TbShippments)
+                .HasForeignKey(d => d.ShippingTypeId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_TbShippments_TbShippingTypes");
+        });
+
+        modelBuilder.Entity<TbShippmentStatus>(entity =>
+        {
+            entity.ToTable("TbShippmentStatus");
+
+            entity.Property(e => e.Id).HasDefaultValueSql("(newid())");
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.UpdatedDate).HasColumnType("datetime");
+
+            entity.HasOne(d => d.Carrier).WithMany(p => p.TbShippmentStatuses)
+                .HasForeignKey(d => d.CarrierId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_TbShippmentStatus_TbCarriers");
+
+            entity.HasOne(d => d.Shippment).WithMany(p => p.TbShippmentStatuses)
+                .HasForeignKey(d => d.ShippmentId)
+                .HasConstraintName("FK_TbShippmentStatus_TbShippments");
+        });
+
+        modelBuilder.Entity<TbSubscriptionPackage>(entity =>
+        {
+            entity.Property(e => e.Id).HasDefaultValueSql("(newid())");
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.PackageName).HasMaxLength(200);
+            entity.Property(e => e.UpdatedDate).HasColumnType("datetime");
+        });
+
+        modelBuilder.Entity<TbUserReceiver>(entity =>
+        {
+            entity.Property(e => e.Id).HasDefaultValueSql("(newid())");
+            entity.Property(e => e.Address).HasMaxLength(500);
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.Email).HasMaxLength(200);
+            entity.Property(e => e.Phone).HasMaxLength(200);
+            entity.Property(e => e.ReceiverName).HasMaxLength(200);
+            entity.Property(e => e.UpdatedDate).HasColumnType("datetime");
+
+            entity.HasOne(d => d.City).WithMany(p => p.TbUserReceivers)
+                .HasForeignKey(d => d.CityId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_TbUserReceivers_TbCities");
+        });
+
+        modelBuilder.Entity<TbUserSender>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK_TbUserSebders");
+
+            entity.Property(e => e.Id).HasDefaultValueSql("(newid())");
+            entity.Property(e => e.Address).HasMaxLength(500);
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.Email).HasMaxLength(200);
+            entity.Property(e => e.Phone).HasMaxLength(200);
+            entity.Property(e => e.SenderName).HasMaxLength(200);
+            entity.Property(e => e.UpdatedDate).HasColumnType("datetime");
+
+            entity.HasOne(d => d.City).WithMany(p => p.TbUserSenders)
+                .HasForeignKey(d => d.CityId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_TbUserSebders_TbCities");
+        });
+
+        modelBuilder.Entity<TbUserSubscription>(entity =>
+        {
+            entity.Property(e => e.Id).HasDefaultValueSql("(newid())");
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.SubscriptionDate).HasColumnType("datetime");
+            entity.Property(e => e.UpdatedDate).HasColumnType("datetime");
+
+            entity.HasOne(d => d.Package).WithMany(p => p.TbUserSubscriptions)
+                .HasForeignKey(d => d.PackageId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_TbUserSubscriptions_TbSubscriptionPackages");
+        });
+
+        //OnModelCreatingPartial(modelBuilder);
+    }
+
+    //private partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ DAL/Migrations/20250603140436_intialMigration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc/>
+    public partial class intialMigration : Migration
+    {
+        /// <inheritdoc/>
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        }
+
+        /// <inheritdoc/>
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
